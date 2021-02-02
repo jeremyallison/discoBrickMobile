ccfe2642609c17b82d7afe23652c92fbe162ac51
Jeremy ALLISON <j.c.allison@gmail.com>
2021-02-02 13:57:48 +0100

    added image assets for android & iOS

--- android/app/src/main/java/com/discobrickmobile/generated/BasePackageList.java
+++ android/app/src/main/java/com/discobrickmobile/generated/BasePackageList.java
@@ -7,10 +7,10 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
+        new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
-        new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.permissions.PermissionsPackage()
     );
   }
